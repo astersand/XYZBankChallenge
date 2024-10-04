b0e3c3c2fdb3a429d8bce9ddbdff4c747f3d6508
uros.simic <uros.simic@clarivate.com>
2024-10-05 00:08:35 +0200

    added settings for screenshots in reports

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,16 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from 'mochawesome/addContext';
+import 'cypress-mochawesome-reporter/register';
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+      const screenshot = `screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+      addContext({ test }, screenshot);
+    }
+    // const video = `assets/videos/${Cypress.spec.name}.mp4`;
+    // addContext({ test }, video);
+  });
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
