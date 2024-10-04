7fcaa5b540824e070415340be16e5f3385bad322
uros.simic <uros.simic@clarivate.com>
2024-10-04 13:33:29 +0200

    added addCustomerPage file with customer fields and buttons

---
+++ cypress/e2e/Selectors/addCustomerPageSelectors.js
@@ -0,0 +1,6 @@
+export default {
+    firstName: 'input[ng-model="fName"]',
+    lastName: 'input[ng-model="lName"]',
+    postCode: 'input[ng-model="postCd"]',
+    addCustomerButton: 'button[type="submit"]'
+}
