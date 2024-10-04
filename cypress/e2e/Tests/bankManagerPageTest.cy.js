87a503af92c8c9cbd6edfa9a1ee366df95b04fa2
uros.simic <uros.simic@clarivate.com>
2024-10-04 19:48:03 +0200

    added more tests on bank manager page

--- cypress/e2e/Tests/bankManagerPageTest.cy.js
+++ cypress/e2e/Tests/bankManagerPageTest.cy.js
@@ -2,15 +2,20 @@ import AddCustomerPage from '../Pages/addCustomerPage';
 import BankManagerHomepage from '../Pages/bankManagerHomePage';
 import Homepage from '../Pages/homepage';
 import { faker } from '@faker-js/faker';
+import OpenAccountPage from '../Pages/openAccountPage';
+import CustomersPage from '../Pages/customersPage';
 
 describe('Bank manager page test', () => {
     const homepage = new Homepage();
     const bankManagerHomepage = new BankManagerHomepage();
     const addCustomerPage = new AddCustomerPage();
+    const openAccountPage = new OpenAccountPage();
+    const customersPage = new CustomersPage();
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
     const postCode = faker.location.zipCode();
-    beforeEach('Open homepage', () => {
+    let accountNumber;
+    before('Open homepage', () => {
         cy.intercept('GET', 'https://www.globalsqa.com/angularJs-protractor/BankingProject/options.html').as('homepage');
         cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
         cy.wait('@homepage');
@@ -31,4 +36,31 @@ describe('Bank manager page test', () => {
             return true;
         });
     })
+    it('Should create account', () => {
+        bankManagerHomepage.openAccountButton.click();
+        openAccountPage.customer.select(`${firstName} ${lastName}`);
+        openAccountPage.currency.select('Dollar');
+        openAccountPage.processButton.click();
+        cy.on('window:alert', (alertText) => {
+            expect(alertText).to.contain('Account created successfully with account Number');
+            accountNumber = alertText.split(' :')[1];
+            return true;
+        });
+    });
+    it('Should search for customers succesfully', () => {
+        bankManagerHomepage.customersButton.click();
+        customersPage.searchField.type(lastName);
+        customersPage.rowCount().should('eq', 1);
+        customersPage.getTextFromCell(0, 0).should('eq', firstName);
+        customersPage.getTextFromCell(0, 1).should('eq', lastName);
+        customersPage.getTextFromCell(0, 2).should('eq', postCode);
+        customersPage.getTextFromCell(0, 3).should('contains', accountNumber);
+        customersPage.searchField.clear();
+    });
+    it('Should delete the customer', () => {
+        customersPage.searchField.type(lastName);
+        customersPage.deleteCustomer(0);  
+        customersPage.searchField.clear();
+        customersPage.rowCount().should('eq', 5);
+    });
 });
